4bd1e969e4cb79b60889fd886dd97b474905b80a
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2025-10-23 20:15:55 +0700

    adding Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { footerLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you.{" "}
+        </p>
+        <img src="/logo.svg" alt="Apple Logo" />
+      </div>
 
-export default Footer
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({ link, label }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
